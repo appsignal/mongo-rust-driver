32d807f2168fa2628dfbef71195f4a25ac32445e
Thijs Cadier <thijs@appsignal.com>
2015-12-14 22:32:01 +0100

    Bump C driver to 1.3.0

--- mongoc-sys/build.rs
+++ mongoc-sys/build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::path::Path;
 use std::process::Command;
 
-static VERSION: &'static str = "1.2.3"; // Should be the same as the version in the manifest
+static VERSION: &'static str = "1.3.0"; // Should be the same as the version in the manifest
 
 fn main() {
     let out_dir_var = env::var("OUT_DIR").unwrap();
