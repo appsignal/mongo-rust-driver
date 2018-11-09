bdc045eeb2dd0e16d40103e75ca2d8e1521c42a0
inv2004 <gPoiuhjkl1>
2018-11-09 02:32:47 -0500

    ChangeStream test in_progress

--- src/change_stream.rs
+++ src/change_stream.rs
@@ -1,7 +1,15 @@
 //! Access to a MongoDB change stream.
+
+use std::ptr;
+use std::iter::Iterator;
 use super::collection::Collection;
 
 use mongoc::bindings;
+use bson::{Bson,Document};
+use super::bsonc::Bsonc;
+use super::BsoncError;
+use super::Result;
+
 
 pub struct ChangeStream<'a> {
     _collection: &'a Collection<'a>,
@@ -19,7 +27,60 @@ impl<'a> ChangeStream<'a> {
             inner
         }
     }
+
+    fn error(&self) -> BsoncError {
+        assert!(!self.inner.is_null());
+        let mut error = BsoncError::empty();
+        let mut reply = Bsonc::new();
+
+        unsafe {
+            bindings::mongoc_change_stream_error_document(
+                self.inner,
+                error.mut_inner(),
+                reply.mut_inner()
+            )
+        };
+        error
+    }
 }
+
+impl<'a> Iterator for ChangeStream<'a> {
+    type Item = Result<Document>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+        let mut bson_ptr: *const bindings::bson_t = ptr::null();
+
+        let success = unsafe {
+            bindings::mongoc_change_stream_next(
+                self.inner,
+                &mut bson_ptr
+            )
+        };
+
+        if success == 1 {
+            assert!(!bson_ptr.is_null());
+            println!("DEBUG0: {}", success);
+
+            let bsonc = Bsonc::from_ptr(bson_ptr);
+            match bsonc.as_document() {
+                Ok(document) => return Some(Ok(document)),
+                Err(error)   => return Some(Err(error.into()))
+            }
+        } else {
+            println!("DEBUG1: {}", success);
+            let error = self.error();
+            println!("DEBUG11: {}", error);
+            if error.is_empty() {
+                None
+            } else {
+                Some(Err(error.into()))
+            }
+        }
+
+    }
+}
+
 
 impl<'a> Drop for ChangeStream<'a> {
     fn drop(&mut self) {
