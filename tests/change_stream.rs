fa3617e8f8d005c55e963f5a2d31f4f92cf33f4f
inv2004 <gPoiuhjkl1>
2018-11-09 03:19:12 -0500

    added $match into test

--- tests/change_stream.rs
+++ tests/change_stream.rs
@@ -17,14 +17,15 @@ fn test_change_stream() {
     let guard = thread::spawn(move || {
         let client     = cloned_pool.pop();
         let collection = client.get_collection("rust_driver_test", "change_stream");
-        let stream = collection.watch(&doc!{}, &doc!{"maxAwaitTimeMS": 1_000}).unwrap();
-        let mut counter = 0;
+        let stream = collection.watch(&doc!{"$match": {"$gte": 10}}, &doc!{"maxAwaitTimeMS": 1_000}).unwrap();
+
+        let mut counter = 10;
         for x in stream {
             let c = x.unwrap().get_document("fullDocument").unwrap().get_i32("c").unwrap();
             if c == counter {
                 counter += 1;
             }
-            if counter == 15 {
+            if counter == 25 {
                 break;
             }
         };
@@ -33,11 +34,11 @@ fn test_change_stream() {
 
     thread::sleep(Duration::from_millis(100));
 
-    for i in 0..15 {
+    for i in 0..25 {
         collection.insert(&doc! {"c": i}, None).unwrap();
     }
 
 
-    assert_eq!(15, guard.join().unwrap());
+    assert_eq!(25, guard.join().unwrap());
 }
 
