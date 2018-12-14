b12c64c9d7ce7f7d97b141714608dbe0a20739bc
inv2004 <gPoiuhjkl1>
2018-12-13 23:11:19 -0500

    ::command tests disabled for windows (until mongoc update)

--- tests/cursor.rs
+++ tests/cursor.rs
@@ -88,6 +88,7 @@ fn test_tailing_cursor() {
     assert_eq!(25, guard.join().expect("Thread failed"));
 }
 
+#[cfg(not(target_os = "windows"))]
 #[test]
 fn test_batch_cursor() {
     let uri      = Uri::new("mongodb://localhost:27017/").unwrap();
--- tests/database.rs
+++ tests/database.rs
@@ -1,5 +1,6 @@
 use mongo_driver::client::{ClientPool,Uri};
 
+#[cfg(not(target_os = "windows"))]
 #[test]
 fn test_command() {
     let uri      = Uri::new("mongodb://localhost:27017/").unwrap();
